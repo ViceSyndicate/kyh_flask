4f2ee79112a9cd3086416af2a5302f969b024c89
vice <vice@DESKTOP-EFUTA7E.localdomain>
2022-02-17 13:08:03 +0100

    updated app

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
-
+from app import dynamodb_access
 
 app = Flask(__name__)
 
 
 @app.get('/')
 def index():
-    return render_template('index.html')
+    all_readings = dynamodb_access.get_all_readings()
+    print(all_readings)
+    return render_template('index.html', all_readings=all_readings)
