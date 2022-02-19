d84b2a45cea4fe56b15201acbfe281f195a74867
vice <vice@DESKTOP-EFUTA7E.localdomain>
2022-02-19 20:57:19 +0100

    Added new pages for data display and adding users.

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -9,3 +9,13 @@ def index():
     all_readings = dynamodb_access.get_all_readings()
     print(all_readings)
     return render_template('index.html', all_readings=all_readings)
+
+@app.get('/all_logs')
+def logs():
+    all_logs = dynamodb_access.get_all_rfid_logs()
+    return render_template('all_logs.html', all_logs=all_logs)
+
+@app.get('all_users')
+def users():
+    all_users = dynamodb_access.get_all_users
+    return render_template('all_users.html', all_users)
--- flask/app/dynamodb_access.py
+++ flask/app/dynamodb_access.py
@@ -1,5 +1,4 @@
 import os
-
 import boto3
 
 
@@ -13,5 +12,26 @@ def get_resources():
 def get_all_readings():
     client = get_resources()
     table = client.Table('temp_readings')
+    response = table.scan()
+    return response['Items']
+
+
+def get_all_rfid_logs():
+    client = get_resources()
+    table = client.Table('entry_logs')
+    response = table.scan()
+    return response['Items']
+
+
+def get_recent_rfid_logs():
+    client = get_resources()
+    table = client.table('entry_logs')
+    # TODO Get 20 most recent logs.
+
+
+def get_all_users():
+    client = get_resources()
+    table = client.Table('users')
+    # TODO Filter out password & sensitive data.
     response = table.scan()
     return response['Items']
